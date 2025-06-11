624334c34097093e18beca7f525b900b9b002633
DiegoWriedt <diegowriedt@gmail.com>
2025-06-11 23:16:33 +0000

    upload

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+import pandas as pd
+import plotly.express as px
 import streamlit as st
 
- 
 
 st.header('Lanzar una moneda')
 
- 
+
 
 st.write('Esta aplicación aún no es funcional. En construcción.')
