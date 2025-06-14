13b7642aba8b2a9d71e6793a8828de8b37775935
DiegoWriedt <diegowriedt@gmail.com>
2025-06-14 16:56:54 +0000

    ultimo

--- app.py
+++ app.py
@@ -13,17 +13,17 @@ st.dataframe(df) # Se muestra el df en la aplicacion
 
 
 
-st.write('Histograma calidad vs año')
+st.write('Histograma vehicular relacionado con el año')
 hist_button = st.button('Crear un histograma')
 if hist_button:
      fig_hist = px.histogram(df, x="model_year")
      st.plotly_chart(fig_hist, use_container_width=True) #duda es correcto este codigo, que hace?
-     st.bar_chart(df, x="model_year", color="condition")
+     
 
 
 
 
-st.write('Relación precio/odómetro')
+st.write('Relación Precio/Odómetro')
 price_button = st.button('Crear gráfico precio/marca')
 if price_button:
     fig_dis = st.scatter_chart(df, x='odometer', y='price')
